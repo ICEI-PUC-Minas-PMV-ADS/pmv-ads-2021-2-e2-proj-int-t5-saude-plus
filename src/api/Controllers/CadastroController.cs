fc767160dcc2ab4c8b59d910f78180e916c36b3a
Denison Amaral <denisonamaral@Denisons-Mac-mini.local>
2021-10-31 23:09:10 -0300

    rota post inserir cadastro no banco finalizada

--- src/api/Controllers/CadastroController.cs
+++ src/api/Controllers/CadastroController.cs
@@ -2,9 +2,9 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MyCadastro.Context;
+using MyCadastro.Models;
 
 namespace MyCadastro.Controllers {
-
     [ApiController]
     [Route("api/cadastro")]
     public class CadastroControllers : ControllerBase {
@@ -13,14 +13,19 @@ namespace MyCadastro.Controllers {
         {
             _appDbContext = appDbContext;
         }
-
         [HttpGet]
         public async Task<IActionResult> GetCadastro(){
             return Ok(
                     new {
-                        success = false,
+                        success = true,
                         data = await _appDbContext.Cadastros.ToListAsync()
                 });
+       }
+       [HttpPost]
+       public async Task<IActionResult> PostCadastro(Cadastro Cadastro){
+           _appDbContext.Cadastros.Add(Cadastro);
+           await _appDbContext.SaveChangesAsync();
+           return Ok(new {success = true, data = Cadastro});
        }
     }
 }
